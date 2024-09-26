2a3bd40b93a106da35c96a761182a497d8222fd3
neeraja <neeraja.bhupathi1696@gmail.com>
2024-09-26 18:45:53 +0530

    Added admin Dashboard based on backend code

--- backend-usermanagement/usermanagement/models.py
+++ backend-usermanagement/usermanagement/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
+from django.utils import timezone
 
     
 class CustomUser(models.Model):
@@ -27,6 +28,40 @@ class CustomUser(models.Model):
     
     class Meta:
         db_table = 'users' 
+        managed=False
+class Customer(models.Model):
+    user_id = models.CharField(max_length=8, primary_key=True)
+    user_email = models.EmailField(unique=True)
+    user_first_name = models.CharField(max_length=30)
+    user_middle_name = models.CharField(max_length=30, blank=True)
+    user_last_name = models.CharField(max_length=30)
+    user_dob = models.DateField()
+    user_phone_number = models.BigIntegerField()
+    user_country = models.CharField(max_length=50)
+    user_city = models.CharField(max_length=50)
+    user_address_line_1 = models.CharField(max_length=255)  
+    user_address_line_2 = models.CharField(max_length=255) 
+    user_pin_code = models.BigIntegerField()
+    user_state = models.CharField(max_length=50)  
+    user_profile_photo = models.CharField(max_length=255, blank=True, null=True)
+    user_password = models.CharField(max_length=255)
+    user_type = models.CharField(max_length=50,default='customer')
+    user_old_password = models.CharField(max_length=128, blank=True, null=True)
+    user_joined_date = models.DateTimeField(default=timezone.now, blank=True, null=True)
+    last_login = models.DateTimeField(default=timezone.now,blank=True,null=True)
+    users_daily_limit = models.DecimalField(
+        max_digits=18,
+        decimal_places=2,
+        default=0,
+    )
+    users_monthly_limit = models.DecimalField(
+        max_digits=18,
+        decimal_places=2,
+        default=0,
+    )
+
+    class Meta:
+        db_table = 'users'
         managed=False
 class AdminUser(models.Model):
     user_id = models.CharField(max_length=8, primary_key=True)
